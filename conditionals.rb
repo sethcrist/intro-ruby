31228dfb5fc36e91f158b5ed6b802c5ec3ec1559
Seth Crist <sethcrist50@gmail.com>
2024-03-05 10:59:41 -0600

    first push into ruby

---
+++ conditionals.rb
@@ -0,0 +1,21 @@
+# Create a method called sum_these_numbers which takes two integers as an argument and prints their sum to the screen.
+# Create a method called is_even, which takes a single integer, and which then returns true if the number is even, and false otherwise.
+# Create a method that takes a number as an argument and prints "Valid" if the number is between 1 and 10 (inclusive) and "Invalid" otherwise.
+# Create a method that takes in a string and determines if the string is a palindrome.
+
+
+# As the first user, I can see a prompt in the terminal to enter my name.
+# As the second user, I can see a prompt in the terminal to enter my name.
+# As the first user, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
+# As the second user, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
+# As a user, I can see a message in the terminal depicting which user won the round.
+# As a user, I can see a message in the terminal noting if there was a tie.
+
+
+#   As a developer, you have been tasked with creating the user registration for a secure web site. To test your code, enter the user ID and password in the terminal to find out if they are acceptable. The user ID and password must adhere to the to following criteria:
+# User ID and password cannot be the same.
+# User ID and password must be at least six characters long.
+# Password must contain at least one of: !#$
+# User ID cannot contain the following characters: !#$ or spaces
+# Password cannot be the word "password".
+# User password must contain at least one number.
