f8929f47c5e908f29f16da5b9f6366006ba8b824
Seth Crist <sethcrist50@gmail.com>
2024-03-05 18:17:57 -0600

    finished conditionals

--- blocks.rb
+++ blocks.rb
@@ -1,10 +1,10 @@
 # while - needs starting point and end point, conditions starts as true will eventually become false
 
-num = 1
-while num < 5
-  p num
-  num += 1
-end
+# num = 1
+# while num < 5
+#   p num
+#   num += 1
+# end
 # prints 0 - 5
 
 # METHODS - can take anonymous functions, aka blocks
@@ -13,23 +13,113 @@ end
 #   - {} - single lines
 
 #Times - executes a block a number of times
-8.times do
-  p 'hello world'
-end
+# 8.times do
+#   p 'hello world'
+# end
 # or
 # 8.times { p 'hello world '}
 #prints 'hello world 8 times'
 
-number =10
-number.times do
-  p 'hello world'
-end
+# number =10
+# number.times do
+#   p 'hello world'
+# end
 # prints hello world 10 times
 
 # EACH
 #   - works on ARRAYs, similar to forEach in JS
-nums = [3, 4, 5, 6]
-nums.each do |value|
-  p value * 5
-end
+# nums = [3, 4, 5, 6]
+# nums.each do |value|
+#   p value * 5
+# end
 # prints 15 20 25 30 (no longer an array)
+
+# RANGE
+#   - list of values, start and end point separated by 2 dots, must set to variable to use
+# range = 1..1000
+# p range
+#
+# range.each do |value|
+#   p value
+# end
+# prints 1 - 1000
+#
+# can also use letters
+# letter_range = 'a'..'z'
+# letter_range.each do |value|
+#   p value
+# end
+# prints a - z
+
+# MAP
+# numbers  = 1..10
+# mapped = numbers.map do |value|
+#   value * 3
+# end
+# p mapped
+# prints [3 6 9 12 15 18 21 24 27 30]
+
+
+# SELECT
+#   - like filter in js, return a subset based on condition, also has built in if statement
+# def only_even(array)
+#   array.select do |value|
+#     value.even?
+#   end
+# end
+# p only_even 1..8
+# prints [2 4 6 8]
+
+
+
+
+
+
+
+#   Create a loop that prints the numbers 1 to 20.
+# num = 1
+# while num <= 20
+#   p num
+#   num += 1
+# end
+
+#   Create a loop that prints the numbers 1 to 20 using a different approach than previously used.
+# number = 1..20
+# number.each do |value|
+#   p value
+# end
+
+#   Create a loop that prints only even numbers from 20 to 0.
+# 20.downto(0) do |value|
+#   p value if value.even?
+# end
+
+#   Create a method that takes in an array of numbers and returns a new array with all the numbers multiplied by 5.
+# mult_five = [1, 2, 3, 4, 5]
+# mult_five.each do |value|
+#   p value * 5
+# end
+
+#   Create a method that takes in an array of lowercase words and capitalizes all of the words, permanently modifying the original array.
+# upper_case = ["apple", "orange", "banana"]
+# word_upper_case = upper_case.map  do |value|
+#   p value.upcase
+# end
+# p word_upper_case
+
+
+#   Create a method that takes in an array of numbers and returns the largest number in the array.
+# array = [1, 2, 4, 6, 9, 20, 35, 45, 100]
+# def largest (array)
+#   p array.max
+# end
+# largest(array)
+
+
+#   Create a method that takes in an array of words and returns the word with the least number of characters.
+
+#   Create a method that takes in an array of numbers and returns only the odd numbers sorted from least to greatest.
+
+#   Create a method that takes in an array of strings and returns all the strings in reverse casing. All uppercase letters should be returned lowercased and all lowercased letters should be returned in uppercase letters.
+#   Create a method that will take in an array of numbers and return an array with the remainder of each number when divided by 2.
+#   Create a method that will take in a range of letters and return an array with all the letters capitalized.
--- conditionals.rb
+++ conditionals.rb
@@ -1,15 +1,74 @@
 # Create a method called sum_these_numbers which takes two integers as an argument and prints their sum to the screen.
+# def sum_these_numbers (num1, num2)
+#   num1 + num2
+# end
+# p sum_these_numbers(5, 8)
+
 # Create a method called is_even, which takes a single integer, and which then returns true if the number is even, and false otherwise.
+# def is_even num
+#   if num % 2 == 0
+#     "#{num} is even"
+#   else
+#     "#{num} is odd"
+#   end
+# end
+# p is_even(9)
+# p is_even(8)
+
 # Create a method that takes a number as an argument and prints "Valid" if the number is between 1 and 10 (inclusive) and "Invalid" otherwise.
+# def num_between (num)
+#   if num <= 10 && num >= 1
+#     "#{num} is valid"
+#   else
+#     "#{num} is invalid"
+#   end
+# end
+# p num_between(15)
+
 # Create a method that takes in a string and determines if the string is a palindrome.
+# def palindrome (string)
+#   if string == string.reverse
+#     "#{string} is a palindrome"
+#   else
+#     "#{string} is not a palindrome"
+#   end
+# end
+# p palindrome('racecar')
+# p palindrome('mom')
 
+# ROCK PAPER SCISSORS!!
+# As the first user, I can see a prompt in the terminal to enter my name. ✅
+# As the second user, I can see a prompt in the terminal to enter my name. ✅
+# As the first user, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors". ✅
+# As the second user, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors". ✅
+# As a user, I can see a message in the terminal depicting which user won the round. ✅
+# As a user, I can see a message in the terminal noting if there was a tie. ✅
 
-# As the first user, I can see a prompt in the terminal to enter my name.
-# As the second user, I can see a prompt in the terminal to enter my name.
-# As the first user, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
-# As the second user, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
-# As a user, I can see a message in the terminal depicting which user won the round.
-# As a user, I can see a message in the terminal noting if there was a tie.
+# p 'enter player one user_name'
+# user_one = gets.chomp
+#
+# p 'enter payer two user_name'
+# user_two = gets.chomp
+#
+# p "#{user_one} choose between rock, paper, scissors"
+# user_one_selection = gets.chomp
+#
+# p "#{user_two} choose between rock, paper, scissors"
+# user_two_selection = gets.chomp
+
+# def game_outcome (user_one, user_one_selection, user_two, user_two_selection)
+#   if user_one_selection == user_two_selection
+#     'tie game'
+#   elsif user_one_selection == 'rock' && user_two_selection == 'scissors' ||
+#     user_one_selection == 'paper' && user_two_selection == 'rock' ||
+#     user_one_selection == 'scissors' && user_two_selection == 'paper'
+#     "#{user_one} wins"
+#   else
+#     "#{user_two} wins"
+#   end
+# end
+# p game_outcome(user_one, user_one_selection, user_two, user_two_selection)
+
 
 
 #   As a developer, you have been tasked with creating the user registration for a secure web site. To test your code, enter the user ID and password in the terminal to find out if they are acceptable. The user ID and password must adhere to the to following criteria:
@@ -19,3 +78,30 @@
 # User ID cannot contain the following characters: !#$ or spaces
 # Password cannot be the word "password".
 # User password must contain at least one number.
+
+p "username"
+userid = gets.chomp
+
+p "password"
+password = gets.chomp
+
+def login_info (userid, password)
+  if userid == password
+    'User id and password cannot be the same.'
+  elsif userid.length < 6
+    'User id must be at least six characters long.'
+  elsif password.length < 6
+    'Password must be at least six characters long.'
+  elsif !(/[!#$]/.match?(password))
+    'Password must include at least one of these: !, #, $.'
+  elsif /[!#$\s]/.match?(userid)
+    'User id cannot include !, #, $, or spaces.'
+  elsif password.downcase == 'password'
+    'Password cannot be the word "password".'
+  elsif password.match(/\d/).nil?
+    'Password must contain at least one number'
+  else
+    'User id and password are acceptable'
+  end
+end
+p login_info(userid, password)
